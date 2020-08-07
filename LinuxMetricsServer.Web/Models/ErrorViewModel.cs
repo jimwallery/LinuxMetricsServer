c8ac81a69c7ae2e1b62ee4fee062ddcba320baa0
Jim Wallery <jim.wallery@cloudfusionllc.com>
2020-08-07 12:47:46 -0500

    Initial Commit

---
+++ LinuxMetricsServer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinuxMetricsServer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
